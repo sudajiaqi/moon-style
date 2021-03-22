3636ec2af643c7416305636e6af187cdda4ec1e4
JiaQi <qi.jia@xtransfer.cn>
2021-03-22 20:02:15 +0800

    fix code style

--- src/main/kotlin/com/github/godmoonlight/moonstyle/settings/MoonSettingConfig.kt
+++ src/main/kotlin/com/github/godmoonlight/moonstyle/settings/MoonSettingConfig.kt
@@ -1,14 +1,14 @@
 package com.github.godmoonlight.moonstyle.settings
 
 data class ToJsonConfig(
-        var comment: Boolean = false,
-        var randomValue: Boolean = false,
-        var enumValues: Boolean = false
+    var comment: Boolean = false,
+    var randomValue: Boolean = false,
+    var enumValues: Boolean = false
 )
 
 data class ToYamlConfig(var randomValue: Boolean = false, var enumValues: Boolean = false)
 
 data class MoonSettingConfig(
-        var toJsonConfig: ToJsonConfig = ToJsonConfig(),
-        var toYamlConfig: ToYamlConfig = ToYamlConfig()
+    var toJsonConfig: ToJsonConfig = ToJsonConfig(),
+    var toYamlConfig: ToYamlConfig = ToYamlConfig()
 )
